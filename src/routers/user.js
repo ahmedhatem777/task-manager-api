660e9904ae9c47a9e1bd4fda5aeb80e1d4f943f3
Ahmed Hatem <ahmedhatem777@hotmail.com>
2020-12-25 16:49:27 +0200

    Fixed user creation (token generation)

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,7 +26,7 @@ router.post('/users', async (req, res) => {
     try {
         await user.save();
         sendWelcomeEmail(user.email, user.name);
-        const token = user.generateToken();
+        const token = await user.generateToken();
         res.status(201).send({user, token});
     }
     catch(err){
