27b21cb07dd107402c09d4e5492a5fd4af469dd4
Ahmed Hatem <ahmedhatem777@hotmail.com>
2021-02-12 19:45:34 +0200

    Allow requests from all origins for the development

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 const corsOptions = {
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: '*'
 }
 app.use(cors(corsOptions));
 app.use(express.json());
